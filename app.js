52e2097f751bbcdc3ab767dda582d846251de4f6
hubcarl <hubcarl@126.com>
2017-10-24 19:25:06 +0800

    feat:support react redux and redux server side render

--- app.js
+++ app.js
@@ -2,4 +2,13 @@
 const Resource = require('./lib/resource');
 module.exports = app => {
   app.react.resource = new Resource(app);
+  app.react.renderElement = (reactElement, locals, options) => {
+    if (reactElement.prototype && reactElement.prototype.isReactComponent) {
+      return Promise.resolve(app.react.renderToString(reactElement, locals));
+    }
+    const context = { state: locals };
+    return reactElement(context, options).then(element => {
+      return app.react.renderToString(element, locals);
+    });
+  }
 };
